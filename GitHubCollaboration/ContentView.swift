97da5fc753dc1ea2c5dd3ae887646c6669ab6eb4
Amulya Manoj <amulya30manoj@gmail.com>
2025-05-03 13:21:32 -0700

    Initial Commit

---
+++ GitHubCollaboration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+/Users/amulyamanoj/Desktop/GitHubCollaboration/GitHubCollaboration//  ContentView.swift
+//  GitHubCollaboration
+//
+//  Created by Amulya Manoj on 5/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Amulya Manoj on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
